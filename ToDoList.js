6fe42b8391d9f3d19af060cfa92e055c295df30b
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-07-21 23:04:00 +0500

    update

---
+++ ToDoList.js
@@ -0,0 +1,6 @@
+import inquirer from "inquirer";
+inquirer.prompt({
+    name: "ToDoList",
+    type: 'input',
+    message: 'What you want to add in todo list',
+});
---
+++ ToDoList.ts
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+ 
+inquirer.prompt({
+
+    name:"ToDoList",
+    type:'input',
+    message:'What you want to add in todo list',
+});
